0344cbb65aec5dd573efe201a0c1a3415d6e73ba
Grétar Hjartarson <gretarh17@ru.is>
2017-12-04 16:28:30 +0000

    meira meira meira

--- assignment2.0/main.cpp
+++ assignment2.0/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "SalaryUI.h"
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    SalaryUI salaryUI;
+    salaryUI.mainMenu();
     return 0;
 }
